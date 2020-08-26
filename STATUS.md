216dbf23524a7cea9c5c93ecb922cb6cfd52da44
francis04j <francis04j@gmail.com>
2020-08-26 09:55:17 +0100

    test workflow

--- STATUS.md
+++ STATUS.md
@@ -1,8 +1,9 @@
 TODOS
 
-Update Get Episode controller to use queries
- - USe predicate or expression-builder to do OR operations
+
 Add Github workflows - https://github.com/dotnet-architecture/eShopOnWeb
 Add Gherkin BDD tests - copy from NOrthwind
 Learn and use NGINX
 Watch Les Jackson .Net Core 3.1 MVC course
+Update Get Episode controller to use queries
+ - USe predicate or expression-builder to do OR operations
--- Tests/Api.IntegrationTests/obj/Debug/netcoreapp3.1/Api.IntegrationTests.AssemblyInfo.cs
+++ Tests/Api.IntegrationTests/obj/Debug/netcoreapp3.1/Api.IntegrationTests.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: Microsoft.AspNetCore.Identity.UI.UIFrameworkAttribute("Bootstrap4")]
-[assembly: Microsoft.AspNetCore.Mvc.Testing.WebApplicationFactoryContentRootAttribute("Api, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "C:\\Dev\\Recoup\\Src\\WebApi", "Api.csproj", "0")]
+[assembly: Microsoft.AspNetCore.Mvc.Testing.WebApplicationFactoryContentRootAttribute("Api, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "c:\\Dev\\Recoup\\Src\\WebApi", "Api.csproj", "0")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api.IntegrationTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
